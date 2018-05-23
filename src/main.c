46ee465bcbcd3f8f92eae51dccba2d5ddf4146aa
LilKek <36250069+LilKek@users.noreply.github.com>
2018-05-23 20:51:56 +0700

    main.c

--- src/main.c
+++ src/main.c
@@ -1,50 +1,56 @@
 #include <stdio.h>
-#include "board.h"
-#include guard
-
-unsigned short int board[8][8] = {{21, 22, 23, 24, 25, 23, 22, 21},
-								  {20, 20, 20, 20, 20, 20, 20, 20},
-								  {0, 0, 0, 0, 0, 0, 0, 0},
-								  {0, 0, 0, 0, 0, 0, 0, 0},
-								  {0, 0, 0, 0, 0, 0, 0, 0},
-								  {0, 0, 0, 0, 0, 0, 0, 0},
-								  {10, 10, 10, 10, 10, 10, 10, 10},
-								  {11, 12, 13, 14, 15, 13, 12, 11}};
-
+#include "board_print_plain.h"
 
 int main()
 {
-	FILE *log;
-	log = fopen("./logs/user.log", "w");
-	fclose(log);
-	printf("Добро пожаловать в шахматы. Введите '1' если хотите играть; если хотите отменить игру, введите любое число\n");
-	char i1[3], j1[3];
-	unsigned short int i2, j2;
-	int a;
-	scanf("%d", &a);
-	while (a == 1) {
-		printf("Выберите фигуру\n");
-		scanf("%s", &i1[0]);
-		while ((i1[0] < 'a') || (i1[0] > 'h')) {
-			printf("Ошибка ввода,повторите попытку\n");
-			scanf("%s", &i1[0]);
-		}
-		scanf("%s", &j1[0]);
-		while ((j1[0] < '1') || (j1[0] > '8')) {
-			printf("Ошибка ввода,повторите попытку\n");
-			scanf("%s", &j1[0]);
-		}
-		
-		i2 = table_1(i1[0]);//Преобразование символа в число
-		j2 = table_2(j1[0]);//Преобразование символа в число
-		log = fopen("./logs/user.log", "a+");
-		fprintf(log, "%c%c%c",code_to_letter(j2, i2), i1[0], j1[0]);
-		fclose(log);
-		print_board();
-		board_(j2, i2);//Вызов основной функции
-		print_board();
-		printf("Для продолжения введите '1' если хотите играть,если хотите отменить игру, введите любое число ");
-		scanf("%d", &a);
-	}
-	return 0;
+    char i, i1;
+    int *index, f = 1, j, j1;
+    int win = 0, count = 0, colour = 0, check_move;
+    
+    board *a = (board *)malloc(sizeof(board) * 8 * 8);
+
+    printf("Insert data format: E2 E4\n");
+    initial_board(a);
+    print_board(a);
+    while (win == 0) {
+        while (colour == 0) {
+            while ((check_move == 0) && (colour == 0)) {
+                if (count % 2 == 0) {
+                    printf ("White figures:");
+                }
+                else
+                    printf("Black figures:");
+                while (f != 0) {
+                    scanf("%c%d %c%d", &i, &j, &i1, &j1);
+                    while(getchar() != '\n');
+                    f = coordinates(i, j, i1, j1);
+                }
+                f = 1;
+        	index = convert(i, j, i1, j1);
+                i = index[0];
+        	j = index[1];
+        	i1 = index[2];
+        	j1 = index[3];
+                colour = colour_checking(a, j, i, count % 2);
+                check_move = move_checking(a, j, i, j1, i1);
+                if (check_move == 0) {
+                    printf("The move is not possible\n");
+                }
+                if (((check_move == 1) && (colour == 0)) || ((check_move == 0) && (colour == 1))) {
+                    check_move = 0;
+                    colour = 0;
+                }
+            }
+        }
+        if (check_move == 1) {
+            move(a, i, j, i1, j1);
+        }
+        print_board(a);
+        count++;
+        check_move = 0;
+        colour = 0;
+        win = win_checking(a);	
+    }
+    free(a);
+    return 0;
 }
